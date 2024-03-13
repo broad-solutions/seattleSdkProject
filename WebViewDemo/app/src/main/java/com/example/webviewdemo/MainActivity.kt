d274ccb51a724f4ad77604d8821bbee9a7dd8ab4
Liuyan <17600421209@163.com>
2024-03-13 18:38:49 +0800

    111

--- WebViewDemo/app/src/main/java/com/example/webviewdemo/MainActivity.kt
+++ WebViewDemo/app/src/main/java/com/example/webviewdemo/MainActivity.kt
@@ -1,49 +1,11 @@
 package com.example.webviewdemo
 
-import android.content.Intent
-import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import com.example.seattlesdk.AnalyticsDelegate
-import com.example.seattlesdk.AnalyticsDelegateImpl
-import com.example.webviewdemo.databinding.ActivityMainBinding
+import android.os.Bundle
 
-class MainActivity : AppCompatActivity(), AnalyticsDelegate by AnalyticsDelegateImpl() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.clickZone.setOnClickListener {
-            val intent= Intent(this, second::class.java)
-            startActivity(intent)
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.FirstFragment)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        setContentView(R.layout.activity_main)
     }
 }
