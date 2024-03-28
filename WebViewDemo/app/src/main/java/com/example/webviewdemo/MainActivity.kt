f68ada6c0697ff057732e2f1f6b8b865faf7d45d
Liuyan <17600421209@163.com>
2024-03-28 17:47:18 +0800

    111

--- WebViewDemo/app/src/main/java/com/example/webviewdemo/MainActivity.kt
+++ WebViewDemo/app/src/main/java/com/example/webviewdemo/MainActivity.kt
@@ -17,6 +17,13 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import com.example.seattlesdk.IMASDK
 import com.example.webviewdemo.databinding.ActivityMainBinding
+import com.google.gson.Gson
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class MainActivity : AppCompatActivity(){
 
@@ -30,11 +37,36 @@ class MainActivity : AppCompatActivity(){
         "https://example.com/video2.mp4",
         "https://example.com/video3.mp4"
     )
+    @OptIn(DelicateCoroutinesApi::class)
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val client = OkHttpClient.Builder()
+                .hostnameVerifier { _, _ -> true } // 禁用主机名验证
+                .build()
+
+            val request = Request.Builder()
+                .url("http://clientapi.pubadding.com/oauth/token?client_id=S766494354&client_secret=17fadc2e6b084965ab5e690bf4bd8f6c&grant_type=client_credentials")
+                .build()
+
+            client.newCall(request).execute().use { response ->
+                if (response.isSuccessful) {
+                    val json=response.body?.string()
+                    val gson = Gson()
+                    val accessTokenResponse = gson.fromJson(json, Datautils::class.java)
+                    val token=accessTokenResponse.access_token
+                    print("token:${token}")
+                } else {
+                    println("请求失败: ${response.message}")
+                }
+            }
+        }
+
+
         val videoUrl="https://storage.googleapis.com/gvabox/media/samples/stock.mp4"
 
         IMASDK().SdkStart(this,videoUrl,R.id.videoContentMain,"Banner")
@@ -79,5 +111,6 @@ class MainActivity : AppCompatActivity(){
 
     override fun onDestroy() {
         super.onDestroy()
+        IMASDK().releasePlayer()
     }
 }
